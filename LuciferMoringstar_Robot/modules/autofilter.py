6bf0ed5765c2b1c32bbb9137d6c4bb76980be33a
Jithincr7 <99895069+Jithincr7@users.noreply.github.com>
2022-03-23 15:07:45 +0530

    Update autofilter.py

--- LuciferMoringstar_Robot/modules/autofilter.py
+++ LuciferMoringstar_Robot/modules/autofilter.py
@@ -124,9 +124,8 @@ async def pm_autofilter(client, message):
                     [InlineKeyboardButton(text=f"{filename}", callback_data=f"pmfile#{file_id}")]
                 )
         else:
-            await message.reply_photo(
-                photo=random.choice(BOT_PICS),
-                caption=LuciferMoringstar.ADD_YOUR_GROUP,
+            await message.reply_text(
+                text=LuciferMoringstar.ADD_YOUR_GROUP,
                 reply_markup=InlineKeyboardMarkup([[
                    InlineKeyboardButton("✉️ REQUEST HERE ✉️", url=f"{SUPPORT}")
                    ]]
