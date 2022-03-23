41c467c232599b847399fa0f384814368f0218af
Jithincr7 <99895069+Jithincr7@users.noreply.github.com>
2022-03-23 08:20:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Pythonmoders/CK-BOT.git /LuciferMoringstar-Robot
+  git clone https://github.com/Jithincr7/CK-BOT.git /LuciferMoringstar-Robot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LuciferMoringstar-Robot
